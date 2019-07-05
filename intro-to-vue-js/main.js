586ab494391745f82a9d67a3c43396d42c2e4582
Alamin <im.alamin.mahamud@gmail.com>
2019-07-05 16:18:00 +0600

    list rendering

--- intro-to-vue-js/main.js
+++ intro-to-vue-js/main.js
@@ -9,6 +9,27 @@ var app = new Vue({
     altText: "A Pair of Socks",
     link: "https://google.com",
     quantity: 0,
-    onSale: false
+    onSale: false,
+    details: ["80% cotton", "20% pollyester", "Gender-Neutral"],
+    variants: [
+      {
+        variantID: 1234,
+        variantColor: "red"
+      },
+      {
+        variantID: 12345,
+        variantColor: "green"
+      }
+    ],
+    sizes: [
+      {
+        id: 1234,
+        value: "XL"
+      },
+      {
+        id: 12345,
+        value: "XXL"
+      }
+    ]
   }
 });

5eb3b58e7ee76b25309ffa35cc94ae0bab2c9125
Alamin <im.alamin.mahamud@gmail.com>
2019-07-05 13:06:13 +0600

    intro to vue js attribute binding

--- intro-to-vue-js/main.js
+++ intro-to-vue-js/main.js
@@ -4,6 +4,9 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "A pair of warm, fuzzy socks"
+    description: "A pair of warm, fuzzy socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    altText: "A Pair of Socks",
+    link: "https://google.com"
   }
 });
